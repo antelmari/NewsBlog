eac1894860de371b4b70f897533897a3407cb24a
Maria <mari200697@yandex.ru>
2023-04-03 00:05:27 +0300

    новостной портал

--- src/app/components/top-headlines/top-headlines.component.ts
+++ src/app/components/top-headlines/top-headlines.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NewsapiService } from '../../service/newsapi.service';
 
 @Component({
